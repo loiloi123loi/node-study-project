87a0e6e2aaa1760b7060984f030965d07b83f44b
Lợi <loivantran10012002@gmail.com>
2023-08-09 23:25:56 +0700

    loi up 09

---
+++ 09_stripe_payment/final/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+// controllers
+const stripeController = require('./controllers/stripeController');
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.use('*', (req, res, next) => {
+    console.log('URL: ', req.url);
+    console.log('METHOD: ', req.method);
+    next();
+});
+
+// stripe
+app.post('/stripe', stripeController);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        await app.listen(
+            port,
+            console.log(`Server listenning on port ${port}...`)
+        );
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+start();
