c4d187088e206d3ad2818549b7c48847b0e87132
Lợi <loivantran10012002@gmail.com>
2023-08-09 12:33:10 +0700

    loi up 07

---
+++ 07_file_upload/final/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    createProduct,
+    getAllProducts,
+} = require('../controllers/productController');
+const {
+    uploadProductImage,
+    uploadProductImageLocal,
+} = require('../controllers/uploadsController');
+
+router.route('/').post(createProduct).get(getAllProducts);
+router.route('/uploads').post(uploadProductImageLocal);
+
+module.exports = router;
